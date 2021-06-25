b5054cc05c428d643c5657fcebd2e29902872be5
Jayesh Dutt Bohra <jayeshduttbohra@gmail.com>
2021-06-25 14:05:51 +0530

    Added basic strucrure and no scroll feature

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+document.querySelector('#explore').addEventListener('click',function(){
 
+document.querySelector('.book-container').scrollIntoView({behavior: 'smooth'});
+})
+document.querySelector('.Home').addEventListener('click',function(){
+
+    document.querySelector('.container-main').scrollIntoView({behavior: 'smooth'});
+    })
