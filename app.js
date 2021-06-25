1abfd24e3178b0095bbf91836870da52e0918973
Jayesh Dutt Bohra <jayeshduttbohra@gmail.com>
2021-06-25 19:50:05 +0530

    Made basic structure of book and added z-index to them

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Scrolling to the next segment js.
+
 document.querySelector('#explore').addEventListener('click',function(){
 
 document.querySelector('.book-container').scrollIntoView({behavior: 'smooth'});
@@ -5,4 +7,29 @@ document.querySelector('.book-container').scrollIntoView({behavior: 'smooth'});
 document.querySelector('.Home').addEventListener('click',function(){
 
     document.querySelector('.container-main').scrollIntoView({behavior: 'smooth'});
-    })
+})
+
+//End of that segment
+
+//........
+
+//Assigning z-index to each page
+
+let pages = document.querySelectorAll('.page');
+
+for(let i=0;i<pages.length;i++)
+{
+    pages[i].style.zIndex = `${pages.length - i}`;
+}
+
+//Completed
+
+//.....
+
+//Applying the 3d effect
+let i=0;
+document.querySelector('.book').addEventListener('click',function(){
+    let page = pages[i];
+    page.classList.add('palat');
+    i++;
+})
