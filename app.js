4f21476a7f3cd6139d360dbee2da013238f236be
Jayesh Dutt Bohra <jayeshduttbohra@gmail.com>
2021-08-02 16:16:20 +0530

    Added space for television section

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let topbut = document.querySelector('.top');
 
 document.querySelector('#explore').addEventListener('click',function(){
 
-document.querySelector('.book-container').scrollIntoView({behavior: 'smooth'});
+document.querySelector('.tv-container').scrollIntoView({behavior: 'smooth'});
 
 
 })
@@ -15,81 +15,6 @@ document.querySelector('.Home').addEventListener('click',function(){
     document.querySelector('.banner').scrollIntoView({behavior: 'smoooth'});
 })
 
-//End of that segment
-
-//........
-
-//Assigning z-index to each page
-
-let pages = document.querySelectorAll('.page');
-document.querySelector('.front-page').style.zIndex = `${pages.length+1}`;
-for(let i=pages.length-1;i>=0;i--)
-{
-    pages[i].style.zIndex = i;
-}
-
-//Completed
-
-//.....
-
-//Applying the 3d effect
-let i=4,bookcount =0 ;
-document.querySelector('.book').addEventListener('click',function(){
-
-    //If the click is first time the book gets shifted.
-        $(".book").css("boxShadow", "none");    //removing box shadow because it's causing a bug and i don't know why...
-        $(".book").css("filter", "none");
-        if(bookcount === 0)
-    {
-        document.querySelector('.book').classList.add('endofbook');
-        document.querySelector('.book').classList.add('shift');
-        document.querySelector('.front-page').classList.add('palat');
-        let tout = 0;
-        for(let j=0;j<5;j++)
-        {
-            setTimeout(function()
-            {
-            let page = pages[j];
-            page.classList.add('palat');        
-            },200+tout);
-            tout+=200;
-        }
-        let audiovar = document.querySelector('#flipover')
-        function playAudio() {
-            audiovar.play();
-          }
-          playAudio();
-        bookcount++;
-    }
-
-    //else the pages shifts as designs.
-
-    else if(i === pages.length-1)
-    {
-        document.querySelector('.book').classList.remove('flip');
-        document.querySelector('.front-page').classList.remove('palat');
-
-        for(let j=0;j<pages.length;j++)
-        {
-            pages[j].classList.remove('palat');
-        }
-        document.querySelector('.book').classList.add('endofbook');
-        bookcount = 0;
-        i = 0;
-    }
-
-    else {
-    let page = pages[i];
-    page.classList.add('palat');
-    let audiovar = document.querySelector('#flip')
-        function playAudio() {
-            audiovar.play();
-          }
-          playAudio();
-          i++;
-    }
-
-})
 //Adding animation of navbar going up if clicks on skillls
 let skills = document.querySelector('.Skills');
 skills.addEventListener('click',()=>{
