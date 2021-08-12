2b7e1de153ff5be29d70735027d434416bb854bd
Jayesh Dutt Bohra <jayeshduttbohra@gmail.com>
2021-08-13 00:28:01 +0530

    Added In development to tvdiv

--- app.js
+++ app.js
@@ -101,3 +101,8 @@ close.addEventListener('click',()=>{
 })
 //Done adding button.
 
+//tv container
+let power = function() {
+    let body = document.getElementById("wrapper");
+    body.className = (body.className == "on") ? "off" : "on";
+}
