b21437c2e86831a8819551ef735c6ffa91243a98
BohraDJayesh <jayeshduttbohra@gmail.com>
2021-12-07 23:12:35 +0530

    Solved Background clipping problem

--- app.js
+++ app.js
@@ -15,25 +15,6 @@ for(let i = 0;i<319;i++) //Inserting all the divs in container div.
 let hex = document.querySelectorAll('.hexdiv'); //object containing all the hexdivs so that
 let insidehex = document.querySelectorAll('.insidehex');//I can select and resize randomly.
 
-function hexsize(randno)
-{
-    insidehex[randno].style.width = "71px";
-    insidehex[randno].style.height = "84.0711px";
-}
-// let hrtime = 1000;
-function hexresize() //function to resize hexagons calling itself at particular time interval.
-{
-    let randno = Math.floor((Math.random() * 317) + 1);
-    insidehex[randno].style.width = "68px";
-    insidehex[randno].style.height = "80px";
-
-    // setTimeout(hexsize(randno),3000);//calling function to bring back actual size of that hexagon.
-    
-    setInterval(hexresize,2000);
-}
-
-  hexresize(); //Calling that function to start the hexagonal effect.
-
 let colorrgb2 = ()=>{
     let randno = Math.floor((Math.random() * 317) + 1);
     setTimeout(() => {$(`.insidehex${randno}`).css("background-color","#F6EB14")},200);
